f1dc78a7e284923af795c2736a171746075f4d3a
bhighnam <bhighnam@gmail.com>
2014-05-06 15:24:52 -0400

    ch.3 completed

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+
+  # performs all of the behind the scenes functions for the home.erb.html file
+  def home
+  end
+  
+  # performs all of the behind the scenes functions for the help.erb.html file
+  def help
+  end
+end
