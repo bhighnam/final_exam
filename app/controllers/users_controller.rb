cf5b749b6da23e3a132425daa7dd517821dbdda0
bhighnam <bhighnam@gmail.com>
2014-05-09 14:42:05 -0400

    finished hartl ch.7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+      @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -28,8 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
