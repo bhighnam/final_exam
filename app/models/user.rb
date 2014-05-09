c881647ba601d00ca6ed5435fec28e623ab6f0c0
bhighnam <bhighnam@gmail.com>
2014-05-09 12:13:54 -0400

    Did hartl ch.6 final exam review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-    # creating a 1 to many association that one user can have many microposts
-    has_many :microposts
-end
+   # checks if the data entered in for the name attribute is 50 characters long and the text area is not blank before entering into the db
+   before_save { self.email = email.downcase }
+   validates :name,  presence: true, length: { maximum: 50 }
+   
+   # checks if the data entered in for the email attribute is there before being entered into the db 
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+   
+    has_secure_password
+  validates :password, length: { minimum: 6 }
+
+end # end of User < ActiveRecord::Base
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 FinalExam::Application.routes.draw do
   
+  
   resources :microposts
 
   resources :users
   
   root to: 'static_pages#home'
+  match '/signin',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
