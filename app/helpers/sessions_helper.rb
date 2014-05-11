c80bc18e741f9d3fc53a0499111a688dbfb918f9
bhighnam <bhighnam@gmail.com>
2014-05-11 15:37:15 -0400

    Hopefully fixed issues in hartl listing 9.15

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,13 +9,13 @@ module SessionsHelper
 
    end # end of method sign_in
    
-   def current_user=(user)
-    @current_user = user
-  end
-  
-  def current_user
+   def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
   end
   
   def signed_in?
