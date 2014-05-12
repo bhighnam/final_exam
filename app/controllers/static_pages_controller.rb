0fd0e0a8d442b576b4f55a5e1c7232732a069bf4
bhighnam <bhighnam@gmail.com>
2014-05-12 19:18:55 -0400

    Hartl listing 10.30 to 10.34

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 
   # performs all of the behind the scenes functions for the home.erb.html file
   def home
+         @micropost = current_user.microposts.build if signed_in?
   end
   
   # performs all of the behind the scenes functions for the help.erb.html file
